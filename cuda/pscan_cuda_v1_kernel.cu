41b0f7474acca45592f745b43fcf0bb8b10323d5
John Ryan <johnryan465@gmail.com>
2024-01-04 23:30:59 +0000

    Added streams

--- cuda/pscan_cuda_v1_kernel.cu
+++ cuda/pscan_cuda_v1_kernel.cu
@@ -5,16 +5,31 @@
 #include <cub/cub.cuh>
 #include <ATen/cuda/CUDAContext.h>
 
-
 #include <vector>
 
+#define CHECK_CUDA_ERROR(val) check((val), #val, __FILE__, __LINE__)
+template <typename T>
+void check(T err, const char *const func, const char *const file,
+           const int line)
+{
+    if (err != cudaSuccess)
+    {
+        std::cerr << "CUDA Runtime Error at: " << file << ":" << line
+                  << std::endl;
+        std::cerr << cudaGetErrorString(err) << " " << func << std::endl;
+        std::exit(EXIT_FAILURE);
+    }
+}
 
-template <typename T> struct PairScalar;
-template <> struct PairScalar<float>
+template <typename T>
+struct PairScalar;
+template <>
+struct PairScalar<float>
 {
     typedef float2 type;
 };
-template <> struct PairScalar<double>
+template <>
+struct PairScalar<double>
 {
     typedef double2 type;
 };
@@ -23,27 +38,27 @@ template <typename vec_t>
 struct MultAddFunctor
 {
     __device__ __forceinline__
-    vec_t operator()(const vec_t &a, const vec_t &b) const {
+        vec_t
+        operator()(const vec_t &a, const vec_t &b) const
+    {
         return {a.x * b.x, a.y * b.x + b.y};
     }
 };
 
-
 template <
     typename scalar_t,
     int ITEMS_PER_THREAD,
-    int BLOCK_THREADS
->
+    int BLOCK_THREADS>
 __global__ void pscan_cuda_forward_kernel(
-    scalar_t* A,
-    scalar_t* X,
+    scalar_t *A,
+    scalar_t *X,
     int dim_size,
     int state_size)
-     {
+{
     // block ID
     const int bidx = blockIdx.x;
     const int didx = blockIdx.y;
-    const int tid = threadIdx.x; 
+    const int tid = threadIdx.x;
     const int offset = bidx * dim_size * state_size + didx * state_size + tid * ITEMS_PER_THREAD;
 
     typedef typename PairScalar<scalar_t>::type pair_type;
@@ -51,36 +66,42 @@ __global__ void pscan_cuda_forward_kernel(
     using TempStorageT = typename BlockScanT::TempStorage;
 
     extern __shared__ char smem[];
-    
-    auto& temp_storage = reinterpret_cast<TempStorageT&>(smem);
+
+    auto &temp_storage = reinterpret_cast<TempStorageT &>(smem);
 
     pair_type thread_data[ITEMS_PER_THREAD];
 
-    if (1 + tid * ITEMS_PER_THREAD < state_size){
-        #pragma unroll
-        for (int i = 0; i < ITEMS_PER_THREAD / 2; ++i) {
-            pair_type tmp = reinterpret_cast<pair_type*>(A+offset+i*2)[0];
-            thread_data[i*2].x = tmp.x;
-            thread_data[i*2 + 1].x = tmp.y;
+    if (1 + tid * ITEMS_PER_THREAD < state_size)
+    {
+#pragma unroll
+        for (int i = 0; i < ITEMS_PER_THREAD / 2; ++i)
+        {
+            pair_type tmp = reinterpret_cast<pair_type *>(A + offset + i * 2)[0];
+            thread_data[i * 2].x = tmp.x;
+            thread_data[i * 2 + 1].x = tmp.y;
         }
-        #pragma unroll
-        for (int i = 0; i < ITEMS_PER_THREAD / 2; ++i) {
-            pair_type tmp = reinterpret_cast<pair_type*>(X+offset+i*2)[0];
-            thread_data[i*2].y = tmp.x;
-            thread_data[i*2 + 1].y = tmp.y;
+#pragma unroll
+        for (int i = 0; i < ITEMS_PER_THREAD / 2; ++i)
+        {
+            pair_type tmp = reinterpret_cast<pair_type *>(X + offset + i * 2)[0];
+            thread_data[i * 2].y = tmp.x;
+            thread_data[i * 2 + 1].y = tmp.y;
         }
     }
     BlockScanT(temp_storage).InclusiveScan(thread_data, thread_data, MultAddFunctor<pair_type>());
-    if (1 + tid * ITEMS_PER_THREAD < state_size){
-        #pragma unroll
-        for (int i = 0; i < ITEMS_PER_THREAD / 2; ++i) {
-            pair_type tmp = reinterpret_cast<pair_type*>(A+offset+i*2)[0];
-            reinterpret_cast<pair_type*>(A+offset+i*2)[0] = {thread_data[i*2].x, thread_data[i*2 + 1].x};
+    if (1 + tid * ITEMS_PER_THREAD < state_size)
+    {
+#pragma unroll
+        for (int i = 0; i < ITEMS_PER_THREAD / 2; ++i)
+        {
+            pair_type tmp = reinterpret_cast<pair_type *>(A + offset + i * 2)[0];
+            reinterpret_cast<pair_type *>(A + offset + i * 2)[0] = {thread_data[i * 2].x, thread_data[i * 2 + 1].x};
         }
-        #pragma unroll
-        for (int i = 0; i < ITEMS_PER_THREAD / 2; ++i) {
-            pair_type tmp = reinterpret_cast<pair_type*>(X+offset+i*2)[0];
-            reinterpret_cast<pair_type*>(X+offset+i*2)[0] = {thread_data[i*2].y, thread_data[i*2 + 1].y};
+#pragma unroll
+        for (int i = 0; i < ITEMS_PER_THREAD / 2; ++i)
+        {
+            pair_type tmp = reinterpret_cast<pair_type *>(X + offset + i * 2)[0];
+            reinterpret_cast<pair_type *>(X + offset + i * 2)[0] = {thread_data[i * 2].y, thread_data[i * 2 + 1].y};
         }
     }
 }
@@ -92,20 +113,20 @@ constexpr std::size_t arch_bytes_size = sizeof(
         cub::BLOCK_SCAN_RAKING /* ALGORITHM */,
         1 /* BLOCK_DIM_Y */,
         1 /* BLOCK_DIM_Z */,
-        ARCH>::TempStorage
-);
+        ARCH>::TempStorage);
 template <typename T, int BLOCK_THREADS, int... Archs>
 constexpr auto archs_max_bytes = (std::max)(
-    {arch_bytes_size<T, BLOCK_THREADS, Archs>...,});
+    {
+        arch_bytes_size<T, BLOCK_THREADS, Archs>...,
+    });
 
 const int TILE_DIM = 32;
 const int BLOCK_ROWS = 8;
 
-template <typename scalar_t>    
+template <typename scalar_t>
 __global__ void transposeNoBankConflicts(scalar_t *odata, const scalar_t *idata, const int stride)
 {
-    __shared__ scalar_t tile[TILE_DIM][TILE_DIM+1];
-
+    __shared__ scalar_t tile[TILE_DIM][TILE_DIM + 1];
 
     int offset = blockIdx.z * stride;
     int x = blockIdx.x * TILE_DIM + threadIdx.x;
@@ -113,74 +134,90 @@ __global__ void transposeNoBankConflicts(scalar_t *odata, const scalar_t *idata,
     int width = gridDim.x * TILE_DIM;
     int height = gridDim.y * TILE_DIM;
 
-    if(x < width){
-        #pragma unroll
-        for (int j = 0; j < TILE_DIM; j += BLOCK_ROWS){
-            if((y+j) < height){
-                tile[threadIdx.y+j][threadIdx.x] = idata[offset + ((y+j)*width) + x];
+    if (x < width)
+    {
+#pragma unroll
+        for (int j = 0; j < TILE_DIM; j += BLOCK_ROWS)
+        {
+            if ((y + j) < height)
+            {
+                tile[threadIdx.y + j][threadIdx.x] = idata[offset + ((y + j) * width) + x];
             }
         }
     }
 
     __syncthreads();
 
-    x = blockIdx.y * TILE_DIM + threadIdx.x;  // transpose block offset
+    x = blockIdx.y * TILE_DIM + threadIdx.x; // transpose block offset
     y = blockIdx.x * TILE_DIM + threadIdx.y;
 
-    if(x < height){
-        #pragma unroll
-        for (int j = 0; j < TILE_DIM; j += BLOCK_ROWS){
-            if((y+j) < width){
-                odata[offset + ((y+j)*height) + x] = tile[threadIdx.x][threadIdx.y + j];
+    if (x < height)
+    {
+#pragma unroll
+        for (int j = 0; j < TILE_DIM; j += BLOCK_ROWS)
+        {
+            if ((y + j) < width)
+            {
+                odata[offset + ((y + j) * height) + x] = tile[threadIdx.x][threadIdx.y + j];
             }
         }
     }
 }
 
+torch::Tensor pscan_cuda_forward(torch::Tensor A, torch::Tensor X)
+{
+    const auto batch_size = A.size(0);
+    const auto state_size = A.size(2);
+    const auto dim_size = A.size(1);
 
+    size_t const num_streams{4};
+    const int offset = (batch_size / num_streams) * state_size * dim_size;
 
-torch::Tensor pscan_cuda_forward(torch::Tensor A, torch::Tensor X) {
-  const auto batch_size = A.size(0);
-  const auto state_size = A.size(2);
-  const auto dim_size = A.size(1);
+    std::vector<cudaStream_t> streams(num_streams);
+    torch::Tensor X_ = torch::empty({X.size(0), X.size(2), X.size(1)}, X.options());
 
-  // Fast transpose
-  
-  torch::Tensor X_ = torch::empty({X.size(0), X.size(2), X.size(1)}, X.options());
+    for (size_t i = 0; i < num_streams; ++i)
+    {
+        CHECK_CUDA_ERROR(cudaStreamCreate(&streams[i]));
+    }
 
-  dim3 dimGrid(dim_size/TILE_DIM, state_size/TILE_DIM, batch_size);
-  dim3 dimBlock(TILE_DIM, BLOCK_ROWS, 1);
+    AT_DISPATCH_FLOATING_TYPES(A.type(), "pscan_transpose_cuda", ([&]
+                                                                  {
+        for (size_t i = 0; i < num_streams; ++i){
+            dim3 dimGrid(dim_size/TILE_DIM, state_size/TILE_DIM, batch_size / num_streams);
+            dim3 dimBlock(TILE_DIM, BLOCK_ROWS, 1);
+            transposeNoBankConflicts<<<dimGrid, dimBlock, 0, streams[i]>>>(
+                X_.data<scalar_t>()+ offset*i,
+                X.data<scalar_t>() + offset*i,
+                X.stride(0)
+            );
+        } }));
 
+    const int threads = 1024;
+    const int elements_per_thread = 2;
 
-    AT_DISPATCH_FLOATING_TYPES(A.type(), "pscan_transpose_cuda", ([&] {
-            transposeNoBankConflicts<<<dimGrid, dimBlock>>>(
-            X_.data<scalar_t>(),
-            X.data<scalar_t>(),
-            X.stride(0)
+    AT_DISPATCH_FLOATING_TYPES(A.type(), "pscan_forward_cuda", ([&]
+                                                                {
+    for(size_t i = 0; i < num_streams; ++i){
+        const auto blocks = dim3(batch_size / num_streams, dim_size, 1);
+
+        typedef typename PairScalar<scalar_t>::type pair_type;
+        auto block_scan_temp_bytes = archs_max_bytes<pair_type, threads, 700, 800, 860>;
+        auto smem_size = (std::max)(1 * sizeof(pair_type), block_scan_temp_bytes);
+    
+        pscan_cuda_forward_kernel<scalar_t, elements_per_thread, threads><<<blocks, threads, smem_size, streams[i]>>>(
+            A.data<scalar_t>() + offset*i,
+            X_.data<scalar_t>() + offset*i,
+            dim_size,
+            state_size
         );
-    }));
+    } }));
 
+    for (size_t i = 0; i < num_streams; ++i)
+    {
+        CHECK_CUDA_ERROR(cudaStreamSynchronize(streams[i]));
+        CHECK_CUDA_ERROR(cudaStreamDestroy(streams[i]));
+    }
 
-
-
-  const int threads = 1024;
-  const int elements_per_thread = 2;
-  const auto blocks = dim3(batch_size, dim_size, 1);
-  auto stream = at::cuda::getCurrentCUDAStream().stream();
-
-
-  AT_DISPATCH_FLOATING_TYPES(A.type(), "pscan_forward_cuda", ([&] {
-    typedef typename PairScalar<scalar_t>::type pair_type;
-    auto block_scan_temp_bytes = archs_max_bytes<pair_type, threads, 700, 800, 860>;
-    auto smem_size = (std::max)(1 * sizeof(pair_type), block_scan_temp_bytes);
-  
-    pscan_cuda_forward_kernel<scalar_t, elements_per_thread, threads><<<blocks, threads, smem_size, stream>>>(
-        A.data<scalar_t>(),
-        X_.data<scalar_t>(),
-        dim_size,
-        state_size
-    );
-  }));
-
-  return X_;
+    return X_;
 }
